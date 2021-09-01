5c4c2b626faac25fa23522523b5d9169682c05f6
Jonathon594 <Jonathon.Owens@outlook.com>
2021-09-01 13:54:56 -0500

    Update EXPConst.java

--- src/main/java/me/Jonathon594/Mythria/Const/EXPConst.java
+++ src/main/java/me/Jonathon594/Mythria/Const/EXPConst.java
@@ -7,9 +7,9 @@ public class EXPConst {
     public static final double FISHING_CATCH = 54;
     public static final double BLOCK_BREAK = 18;
     public static final double BLOCK_PLACE = 18;
-    public static final double STAMINA_USE_TICK = 0.5;
+    public static final double STAMINA_USE_TICK = 0.65;
     public static final float MELEE_DAMAGE = 9;
-    public static final int LIGHT_FIRE_TICK = 4;
+    public static final int LIGHT_FIRE_TICK = 6;
     public static final double COOK_ITEM = 108;
     public static final int ANVIL_WELD = 18;
     public static final int ANVIL_HAMMER_HIT = 9;
